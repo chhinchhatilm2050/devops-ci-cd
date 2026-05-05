d3e33fd65c4fa84e66b3f76b8f16e7d6fdd1046a
Chhin Chhatt <chhinchhat.ilm2050@gmail.com>
2026-05-05 21:34:48 +0700

    continue develop

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ app.get('/users/:id', (req, res) => {
     return res.status(404).json({ error: 'User not found' });
   }
   console.log(user);
+  console.log("helloword");
   res.json({ user });
 });
 
