64aae7512e9651b57238131b66eb83bbbf87f502
Chhin Chhatt <chhinchhat.ilm2050@gmail.com>
2026-05-13 14:26:09 +0700

    run code two inviroment at once

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import express from 'express';
 import globalErrorHandler from './middlewares/globalErrorHandler.js';
 import notFound from './middlewares/notFound.js';
@@ -6,6 +6,19 @@ import globalLimiter from './middlewares/rateLimit.js';
 import requestLogger from './middlewares/logger.js';
 import helmet from 'helmet';
 import router from './routes/index.js';
+import fs from 'fs';
+import path from 'path';
+
+const envFile = `.env.${process.env.NODE_ENV || 'dev'}`;
+const envPath = path.resolve(process.cwd(), envFile);
+
+if (fs.existsSync(envPath)) {
+  dotenv.config({ path: envPath });
+} else {
+  dotenv.config();
+}
+
+console.log(`Current NODE_ENV: ${process.env.NODE_ENV}`);
 
 const app = express();
 app.use(helmet());
@@ -14,8 +27,7 @@ app.use(requestLogger);
 app.use(express.json());
 
 app.get('/health', (req, res) => {
-  console.log('chhatdd');
-  console.log('tori');
+  console.log('Welcome chhinchhat');
   res.status(200).json({
     status: 'healthy',
     timestamp: new Date().toISOString(),
@@ -24,7 +36,6 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/api', router);
-
 app.use(globalErrorHandler);
 app.use(notFound);
 
